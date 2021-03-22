5cdca08841692bf6a4c851bb56e4cfed65ae7a0f
Mateusz <muzykantelektryk@gmail.com>
2021-03-22 20:42:59 +0100

    add loading and saving image, sobel edge detection without any threads

---
+++ src/com/company/ImageBuffer.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class ImageBuffer {
+    public BufferedImage img;
+
+
+    public BufferedImage getImage(File filename) {
+        // This time, you can use an InputStream to load
+        try {
+            img = ImageIO.read(filename);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return img;
+    }
+
+    public void saveImage(File filename) {
+        try {
+            File outputfile = new File(String.valueOf(filename));
+            ImageIO.write(img, "png", outputfile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,19 @@
 package com.company;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("DUPA");
+        ImageBuffer imgbuffer = new ImageBuffer();
+        BufferedImage img = imgbuffer.getImage(new File("test.jpg"));
+
+        EdgeDetection.sobelEdgeDetection(img);
+        imgbuffer.saveImage(new File("save.jpg"));
+
     }
+
+
 }
