39abef1b9aed00da3e4742de7656294a9d0f2b75
Sadaffi <bartek_sadowski@o2.pl>
2021-03-27 18:55:27 +0100

    [code-cleanup] Removed duplicate code from Image Thread

--- src/com/company/ImageThread.java
+++ src/com/company/ImageThread.java
@@ -5,21 +5,14 @@ import java.io.File;
 import java.util.concurrent.CountDownLatch;
 
 public class ImageThread implements Runnable{
+    ImageProcessing imageProcessing;
 
-    String absolutePath;
-    String file;
-    EdgeDetection edgeDetection;
     public ImageThread(String absolutePath, String file) {
-        this.absolutePath=absolutePath;
-        this.file=file;
+        this.imageProcessing = new ImageProcessing(absolutePath, file);
     }
 
     @Override
     public void run() {
-        ImageBuffer imgbuffer = new ImageBuffer();
-        BufferedImage img = imgbuffer.setImage(new File(absolutePath + "input\\" + file));
-        edgeDetection = new EdgeDetection(img);
-        edgeDetection.sobelEdgeDetection();
-        imgbuffer.saveImage(new File(absolutePath + "output\\" + file));
+        imageProcessing.detection();
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -66,19 +66,10 @@ public class Main {
                         }
                     });
                     try {
-                        System.out.println("attempt to shutdown executor");
                         executor.shutdown();
-                        executor.awaitTermination(5, TimeUnit.SECONDS);
-                    }
-                    catch (InterruptedException e) {
-                        System.err.println("tasks interrupted");
                     }
                     finally {
-                        if (!executor.isTerminated()) {
-                            System.err.println("cancel non-finished tasks");
-                        }
-                        executor.shutdownNow();
-                        System.out.println("shutdown finished");
+                        if (!executor.isTerminated());
                     }
                     break;
             }
